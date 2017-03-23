5402d4e4c7c464766173367352076dfbd2e771ba
mendesiasmin <mendesiasmin96@gmail.com>
2017-03-23 09:34:05 -0300

    Criação da model Post

---
+++ Blog/db/migrate/20170323123333_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :posts do |t|
+      t.string :titulo
+      t.string :autor
+      t.string :conteudo
+
+      t.timestamps
+    end
+  end
+end
