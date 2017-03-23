83316cf00e12df1bc203a0e3183054ff3e1c561a
mendesiasmin <mendesiasmin96@gmail.com>
2017-03-23 09:36:09 -0300

    Criação da controller Posts

--- Blog/config/routes.rb
+++ Blog/config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+
+  get 'posts/show'
+
+  get 'posts/update'
+
+  get 'posts/create'
+
+  get 'posts/delete'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
